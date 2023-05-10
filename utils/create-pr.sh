eab298e6ed3db47919f646d10b4cefef6915358b
SeryioGonzalez <sergio.gonzalez.jimenez@gmail.com>
2023-05-10 16:15:30 +0200

    Update create-pr.sh

--- utils/create-pr.sh
+++ utils/create-pr.sh
@@ -35,6 +35,8 @@ repo_url="${DEST_REPO#http://}"
 repo_url="${DEST_REPO#https://}"
 repo_url="https://automated:$TOKEN@$repo_url"
 
+echo "Repo URL is repo_url"
+
 echo "git clone $repo_url -b $DEST_BRANCH --depth 1 --single-branch"
 git clone $repo_url -b $DEST_BRANCH --depth 1 --single-branch
 repo=${DEST_REPO##*/}
